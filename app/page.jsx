b565ba3f00bc268737d481d7c123b6372d3d1f6b
Chantrea-dev <124015861+Chantrea-dev@users.noreply.github.com>
2024-05-02 13:18:08 +0700

    test

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <div className="text-3xl font-bold text-center">
+                <h1>Project smart home</h1>
+                <p>Waiting UI</p>
+            </div>
+        </>
+    );
+}
