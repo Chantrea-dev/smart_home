04683655271b090de49490ef9438fac5bf6fc894
GitSoriya <khloengsoriya@gmail.com>
2024-05-05 16:09:28 +0700

    Change Bg

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <>
             <div>
-                <h1>Project smart home</h1>
+                <h1 className="text-amber-500 bg-red-700">Project smart home</h1>
                 <p className="text-cyan-600 text-center">Waiting UI Processing</p>
             </div>
         </>
